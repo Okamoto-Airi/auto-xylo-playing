074f76b59864aa628f4c275948c612b851332b24
Okamoto-Airi <perc934@gmail.com>
2026-01-29 17:01:30 +0900

    update: 新たに叩き終わりのアームの姿勢を記録し、叩いた後その姿勢に戻すように修正(次の音が離れていると、うまく叩けないため要改善＆テンポの調整が必要)

--- play_music.py
+++ play_music.py
@@ -5,6 +5,7 @@ from lerobot.robots.so_follower import SO101Follower, SO101FollowerConfig
 # ==== 設定 ====
 SCORE_JSON = "score.json"        # ← 楽譜JSON（今回の形式）
 POSE_JSON = "notes_poses.json"  # ← 音名→姿勢対応表
+POSE_OFF_JSON = "notes_poses_off.json"  # ← 音を鳴らした後に戻る姿勢
 PORT = "/dev/ttyACM0"
 ROBOT_ID = "my_awesome_follower_arm"
 
@@ -12,9 +13,8 @@ MOVE_RATIO = 0.4   # 音価のうち「叩きに使う割合」
 LIFT_RATIO = 0.2   # 音価のうち「離れる割合」
 
 LIFT_OFFSET = {
-    "shoulder_lift.pos": -10.0,
-    "elbow_flex.pos": 10.0,
-    "wrist_flex.pos": 15.0,
+    "shoulder_lift.pos": 20.0,
+    "elbow_flex.pos": 20.0
 }
 
 JOINT_NAMES = [
@@ -26,18 +26,31 @@ JOINT_NAMES = [
     "gripper.pos",
 ]
 
+OFF_MOVE_TIME = 0.12  # オフ姿勢へ戻る時間（秒）
+
 
 def main():
     # ==== JSON読み込み ====
+    # 楽譜読み込み
     with open(SCORE_JSON, "r", encoding="utf-8") as f:
         score = json.load(f)
 
+    # 姿勢読み込み
     with open(POSE_JSON, "r", encoding="utf-8") as f:
         raw_note_poses = json.load(f)
+
+    # オフ姿勢読み込み
+    with open(POSE_OFF_JSON, "r", encoding="utf-8") as f:
+        raw_note_poses_off = json.load(f)
 
     note_poses = {
         item["note"]: item["motors"]
         for item in raw_note_poses
+    }
+
+    note_poses_off = {
+        item["note"]: item["motors"]
+        for item in raw_note_poses_off
     }
 
     bpm = score["bpm"]
@@ -69,19 +82,20 @@ def main():
 
             # --- 叩く位置へ ---
             robot.send_action({**target_pos, "time_from_start": move_time})
-            time.sleep(move_time + 0.02)
+            time.sleep(move_time + 1.5)
 
-            # --- 振り上げ ---
-            lift_pos = {}
-            for k in JOINT_NAMES:
-                base = float(target_pos[k])
-                lift_pos[k] = base + LIFT_OFFSET.get(k, 0.0)
-
-            robot.send_action({**lift_pos, "time_from_start": lift_time})
-            time.sleep(lift_time + 0.02)
+            # --- オフ姿勢へ戻す ---
+            off_pos = note_poses_off.get(note)
+            if off_pos is None:
+                # 定義がなければリフト姿勢のまま次へ移動
+                print(f"警告: オフ姿勢が未定義です: {note}（スキップ）")
+                continue
 
             # --- 音価分保持 ---
             time.sleep(max(0.0, hold_time))
+
+            robot.send_action({**off_pos, "time_from_start": lift_time})
+            time.sleep(lift_time + 1.5)
 
     finally:
         robot.disconnect()
